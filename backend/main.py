9244be47569c0a7012c05c68005c189759f31f92
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-02-04 16:01:35 +0100

    Backend now on Flask

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,71 @@
 import webapp2
+from google.appengine.api import taskqueue
+from flask import Flask, render_template, request
+import logging
+
+app = Flask(__name__)
 
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
 
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-], debug=True)
+@app.route("/task")
+def task():
+
+    url = "/run-task"
+    params = {'stuff': 'aadje'}
+    taskqueue.add(queue_name='default', url=url, params=params,
+                  retry_options=taskqueue.TaskRetryOptions(task_retry_limit=0))
+
+    logging.warning("Set a task")
+
+    return ""
+
+
+@app.route("/run-task", methods=['post'])
+def run_task():
+    logging.warning("Running a task")
+
+    return ""
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+
+
+
+# Webapp
+# class MainPage(webapp2.RequestHandler):
+#     def get(self):
+#         self.response.headers['Content-Type'] = 'text/plain'
+#         self.response.write('Hello, World!')
+#
+#
+# class Task(webapp2.RedirectHandler):
+#     def get(self):
+#         self.response.write("Go and set a task")
+#
+#         url = "/run-task"
+#         params = {'stuff': 'aadje'}
+#         taskqueue.add(queue_name='default', url=url, params=params,
+#                       retry_options=taskqueue.TaskRetryOptions(task_retry_limit=0))
+#
+#         logging.warning("Set a task")
+#
+#
+# class RunTask(webapp2.RedirectHandler):
+#     def post(self):
+#         logging.warning("Running a task")
+#
+#
+# app = webapp2.WSGIApplication([
+#     ('/', MainPage),
+#     ('/task', Task),
+#     ('/run-task', RunTask)
+# ], debug=True)
