ee1cb8fb285c160727c3985e3199dbd72a5d4480
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-02-06 12:12:51 +0100

    Download image from cloud storage

--- backend_node/app.js
+++ backend_node/app.js
@@ -5,6 +5,89 @@ app.get('/', (req, res) => {
   res.send('Hello from Aadje... Engine!');
 });
 
+app.get('/storage', (req, res) => {
+  console.log("Checking out storage")
+
+  listBuckets()
+
+  res.send('done')
+})
+
+app.get('/trips/:tripId/images/:imageId.jpg', (req, res) => {
+  // /trips/5P4dof5zt7iGGG9EmZoX/images/FeSDhDOdueIJj.jpg
+
+  console.log("Getting image")
+  getImage(req.params.tripId, req.params.imageId, res)
+  // fs.pipe(res)
+  // res.end()
+
+})
+
+async function listBuckets() {
+  // [START storage_list_buckets]
+  // Imports the Google Cloud client library
+  const {Storage} = require('@google-cloud/storage');
+
+  // Creates a client
+  // const storage = new Storage();
+
+  const storage = new Storage({
+    projectId: 'gwa-net',
+    keyFilename: '../secrets/gwa-net-13e914d23139.json'     // TODO: In production this probably doesn't work. ../secrets is outside scope of app.yaml and won't get uploaded
+  });  
+
+  // Lists all buckets in the current project
+  const [buckets] = await storage.getBuckets();
+  console.log('Buckets:');
+  buckets.forEach(bucket => {
+    console.log(bucket.name);
+  });
+  // [END storage_list_buckets]
+}
+
+// Bucket name: gwa-net.appspot.com
+function getImage(tripId, imageId, res) {
+  // Imports the Google Cloud client library
+  const {Storage} = require('@google-cloud/storage');
+
+  // Creates a client
+  const storage = new Storage({
+    projectId: 'gwa-net',
+    keyFilename: '../secrets/gwa-net-13e914d23139.json'     // TODO: In production this probably doesn't work. ../secrets is outside scope of app.yaml and won't get uploaded
+  });  
+
+  /**
+   * TODO(developer): Uncomment the following lines before running the sample.
+   */
+  const bucketName = 'gwa-net.appspot.com';
+  const srcFilename = `trips/${tripId}/images/${imageId}.jpg`
+  const destFilename = 'test_image.jpg';
+
+  const options = {
+    // The path to which the file should be downloaded, e.g. "./file.txt"
+    destination: destFilename,
+  };
+
+  // Downloads the file
+  // await storage
+  //   .bucket(bucketName)
+  //   .file(srcFilename)
+  //   .download(options);
+
+  var f = storage
+    .bucket(bucketName)
+    .file(srcFilename)
+
+  f.download().then(function(data) {
+    var content = data[0]
+    res.set('Content-Type', 'image/jpg');
+    res.send(content)
+  })
+
+  // res.send()  
+}
+
+
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
