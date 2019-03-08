a2519d5d43106e8f7850defb16cef65d6163cbb8
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-03-08 22:41:56 +0100

    Init website (Vue-cli)

---
+++ website/vue.config.js
@@ -0,0 +1,8 @@
+// vue.config.js
+module.exports = {
+  // options...
+  // publicPath: '/static/',
+  outputDir: '../appengine/default/static',
+  assetsDir: 'static/dist'
+  // indexPath: 'index/index.html'
+}
