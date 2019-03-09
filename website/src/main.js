3b9a76785adffba99db98fa23c10eb93324715f9
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-03-10 00:18:12 +0100

    Cookie consent

--- website/src/main.js
+++ website/src/main.js
@@ -3,6 +3,9 @@ import App from './app.vue'
 import router from './router'
 import { translate as _ } from './system/translator'
 
+// import { CookieConsent } from 'cookieconsent'
+import 'cookieconsent'
+
 Vue.config.productionTip = false
 Vue.prototype._ = _
 
@@ -10,3 +13,28 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+document.addEventListener('DOMContentLoaded', () => {
+  // debugger
+  window.cookieconsent.initialise({
+    palette: {
+      popup: {
+        background: '#ebfeff',
+        text: '#00ACB8'
+      },
+      button: {
+        background: '#00ACB8',
+        text: '#ffffff'
+      }
+    },
+    position: 'bottom',
+    theme: 'classic',
+    content: {
+      message: _('This website uses cookies to ensure you get the best experience on our website'),
+      dismiss: _('Got it!'),
+      link: _('Our privacy policy.'),
+      href: '/privacy',
+      target: '_self'
+    }
+  })
+})
