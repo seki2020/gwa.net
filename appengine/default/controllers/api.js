608e0e4d96da683cf657cecd9be2f9e6be67caab
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-03-12 15:54:54 +0100

    API: Trip delete

--- appengine/default/controllers/api.js
+++ appengine/default/controllers/api.js
@@ -7,6 +7,9 @@ admin.initializeApp({
   databaseURL: 'https://gwa-net.firebaseio.com'
 });
 
+const db = admin.firestore()
+const storage = admin.storage()
+
 // Bucket name: gwa-net.appspot.com
 module.exports.tripDelete = async function (req, res) {
   const tripId = req.params.tripId
@@ -24,36 +27,84 @@ module.exports.tripDelete = async function (req, res) {
       console.log(`- Verified the token for: ${uid}`)
 
       // Find the deleted trip for the user (user can only delete their own trips)
-      
-      // Try to find in the datastore
-      var ref = db.collection('trips');
-      var query = ref.where('user.id', '==', user.id).where('url', '==', event.url).get()
-        .then(snapshot => {
-          if (snapshot.empty) {
-            // Add the event
-            addEvent(req, res, event)
-            
+      // let db = admin.firestore()
+      db.collection('trips').doc(tripId).get()
+        .then(doc => {
+          if (doc.exists) {
+            let data = doc.data()
+            if (data.deleted == true && data.user.id == uid) {
+            // if (data.user.id == uid) {
+              console.log("We can delete this trip")
+
+              // Delete the TripUser document first, because we don't want this Trip to show up anymore
+              db.collection('trips-users').where('trip.id', '==', tripId).get()
+                .then(snapshot => {
+                  if (!snapshot.empty) {
+                    var batch = db.batch();
+                    snapshot.forEach(doc => {
+                      // Delete documents in a batch
+                      batch.delete(doc.ref);
+                    })
+                    batch.commit().then(() => {
+                      console.log(" - Delete users - Done")
+                    });
+                  }
+                })
+                .catch(err => {
+                  console.log('Error getting documents', err);
+                })
+
+              // Delete the TripPosts
+              db.collection('trips-posts').where('trip.id', '==', tripId).get()
+                .then(snapshot => {
+                  if (!snapshot.empty) {
+                    snapshot.forEach(doc => {
+                      // console.log(doc.id, '=>', doc.data());
+                      console.log(` - post: ${doc.id}`);
+
+                      let post = doc.data()
+
+                      // Remove the stored media
+                      for (i=0; i<post.media.length; i++) {
+                        let media = post.media[i]
+                        
+                        const bucketName = 'gwa-net.appspot.com';
+                        const filename = `trips/${tripId}/images/${media.id}.jpg`
+
+                        console.log(`   + media: ${filename}`)
+
+                        var f = storage.bucket(bucketName).file(filename)
+                        f.delete().then(() => {
+                          console.log("     -- Deleted")
+                        }).catch(error => {
+                          console.log(`     -- Error: ${error}`)
+                        })
+
+                      }
+
+                      // Remove the post
+                      doc.ref.delete()
+                    });
+                  }
+              
+                })
+                .catch(err => {
+                  console.log('Error getting documents', err);
+                });
+
+              // Delete the Trip
+              doc.ref.delete()  
+            }
+
           }
         })
         .catch(err => {
-          console.log('Error getting documents', err);
+          console.log('Error getting document', err);
         });
-
-
-
-
-
-
-
-
-
-
-
-
 
     }).catch(function(error) {
       // Handle error
-      console.log("- Trouble verifying the token")
+      console.log(`- Trouble verifying the token: ${error}`)
     });    
   }
   res.json({"foo": "bar"});
