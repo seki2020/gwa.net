10de7a4b7b9298879fff69bd2c47befcd08449df
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-03-17 16:21:56 +0100

    Update cloud function: createTripPost

--- functions/index.js
+++ functions/index.js
@@ -8,19 +8,18 @@ exports.createTripPost = functions.firestore
     .document('trips-posts/{id}')
     .onCreate((document, context) => {
       // Get an object representing the document
-      // e.g. {'name': 'Marie', 'age': 66}
-      const newValue = document.data();
+      const newPost = document.data();
 
       // access a particular field as you would any JS property
-      const message = newValue.message;
-      const updated = newValue.updated
-      const tripId = newValue.trip.id
+      // const message = newValue.message;
+      // const updated = newValue.updated
+      // const tripId = newValue.trip.id
+
+      const media = newPost.media
 
       // Get the TripUser collection?
       const db = admin.firestore()
-
-      var ref = db.collection('trips-users');
-      var query = ref.where('trip.id', '==', tripId).get()
+      db.collection('trips-users').where('trip.id', '==', newPost.trip.id).get()
         .then(snapshot => {
           if (snapshot.empty) {
             console.log('No matching documents.');
@@ -28,12 +27,40 @@ exports.createTripPost = functions.firestore
           }
       
           snapshot.forEach(doc => {
-            console.log(doc.id, '=>', doc.data());
+            const tripUser = doc.data()
+            const recent = tripUser.recent
+            const recentMedia = recent !== undefined ? (recent.media !== undefined ? recent.media : []) : []
+
+            // console.log(doc.id, '=>', tripUser);
+            console.log('Recent media: ', recentMedia)
+
+            // Check for media in the post
+            var newMedia = []
+            // if (media.length > 0) {
+              for (var i=0; i<Math.min(4, media.length); i++) {
+                newMedia.push(media[i])
+              }
+            // } 
+            console.log('New media: ', newMedia)
+            const newLength = newMedia.length
+            const recentLength = recentMedia.length
+            const count = Math.min((4 - newMedia.length), recentMedia.length)
+            console.log('length: ', newLength, ', ', recentLength, 'count ', count)
+
+            for (var j=0; j<count; j++) {
+              console.log(j)
+              newMedia.push(recentMedia[j])
+            }
+            console.log('New media: ', newMedia)
 
             let docRef = db.collection("trips-users").doc(doc.id)
             docRef.update({
-              lastMessage: message,
-              updated: updated
+              recent: {
+                message: newPost.message,
+                user: newPost.user,
+                media: newMedia
+              },
+              updated: newPost.updated
             })
 
           });
