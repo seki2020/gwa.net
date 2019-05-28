8c758d55c64d8bc581d4e5715fad71f52c0d811a
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-05-28 23:13:22 +0200

    Update security rules

--- firebase/firestore-tests/spec/trips-users.spec.js
+++ firebase/firestore-tests/spec/trips-users.spec.js
@@ -5,32 +5,66 @@ const auth = {
 }
 
 const mockData = {
-  'trips/one': {
-    user: {
-      id: 'other'
-    }
-  },
-  'trips-users/xyz': {
+  'trips/me': {
     trip: {
-      id: 'one'
-    },
-    user: {
-      id: 'other'
-    }
-  },
-  'trips/two': {
-    user: {
       id: 'me'
-    }
-  },
-  'trips-users/abc': {
-    trip: {
-      id: 'two'
     },
     user: {
       id: 'me'
     }
   },
+  'trips-users/me_me': {    // trip_user
+    trip: {
+      id: 'me'
+    },
+    user: {
+      id: 'me'
+    }
+  },
+  'trips-users/me_you': {    // trip_user
+    trip: {
+      id: 'me'
+    },
+    user: {
+      id: 'you'
+    }
+  },
+
+  'trips/other': {
+    trip: {
+      id: 'other'
+    },
+    user: {
+      id: 'other'
+    }
+  },
+  'trips-users/other_me': {    // trip_user
+    trip: {
+      id: 'other'
+    },
+    user: {
+      id: 'me'
+    }
+  },
+  'trips-users/other_you': {    // trip_user
+    trip: {
+      id: 'other'
+    },
+    user: {
+      id: 'you'
+    }
+  },
+
+  'trips/public': {
+    privacy: 2,
+    trip: {
+      id: 'public'
+    },
+    user: {
+      id: 'other'
+    }
+  },
+
 };
 
 describe('Not Authorized user', () => {
@@ -44,14 +78,14 @@ describe('Not Authorized user', () => {
     await teardown()
   });
 
-  test('Deny getting trip', async () => {
-    await expect(db.doc('trips-users/xyz').get()).toDeny();
-    await expect(db.doc('trips-users/abc').get()).toDeny();
+  test('Deny getting trip-user', async () => {
+    await expect(db.doc('trips-users/me_me').get()).toDeny();
+    await expect(db.doc('trips-users/me_you').get()).toDeny();
   })
 
-  test('Deny creating trip', async () => {
+  test('Deny creating trip-user', async () => {
     await expect(db.collection('trips-users').add({})).toDeny();
-    await expect(db.doc('trips-users/def').set({trip: {id: 'two'}, user: {id: 'me'}})).toDeny();
+    await expect(db.doc('trips-users/two_me').set({trip: {id: 'two'}, user: {id: 'me'}})).toDeny();
   })
 })
 
@@ -66,47 +100,67 @@ describe('Authorized user', () => {
     await teardown()
   });
 
-  test('Deny access random trip user', async () => {
-    await expect(db.doc('trips-users/xyz').get()).toDeny();
+  // test('Deny getting trip-users of random (not owned) trip', async () => {
+  //   await expect(db.collection('trips-users').where('trip.id', '==', 'other')).toAllow();
+  // })
+  test('Deny getting trip-users of random (not owned) trip', async () => {
+    await expect(db.collection('trips-users').get()).toDeny();
   })
-  test('Deny update random trip user', async () => {
-    await expect(db.doc('trips-users/xyz').update({role: 'follower'})).toDeny();
+  test('Deny access random trip-user', async () => {
+    await expect(db.doc('trips-users/other_you').get()).toDeny();
   })
-
-  test('Allow getting own trip user', async () => {
-    await expect(db.doc('trips-users/abc').get()).toAllow();
+  test('Allow access to trip-user if you own the trip', async () => {
+    await expect(db.doc('trips-users/me_you').get()).toAllow();
   })
-  test('Allow update own trip user', async () => {
-    await expect(db.doc('trips-users/abc').update({role: 'follower'})).toAllow();
-  })
-  test('Deny update user of own trip user', async () => {
-    await expect(db.doc('trips-users/abc').update({role: 'follower', user: {id: 'some'}})).toDeny();
-  })
-  test('Deny update trip of own trip user', async () => {
-    await expect(db.doc('trips-users/abc').update({role: 'follower', trip: {id: 'xyz'}})).toDeny();
+  test('Allow access to trip-user that you are self', async () => {
+    await expect(db.doc('trips-users/other_me').get()).toAllow();
   })
 
-  test('Deny create trip for someone else', async () => {
-    await expect(db.doc('trips-users/hij').set({name: 'four', user: {id: 'someone'}})).toDeny();
-  })
-  test('Deny create trip without trip for yourself', async () => {
-    await expect(db.doc('trips-users/rst1').set({role: 'follower', user: {id: 'me'}})).toDeny();
-  })
-  test('Deny create trip with not existing trip for yourself', async () => {
-    await expect(db.doc('trips-users/rst2').set({role: 'follower', user: {id: 'me'}, trip: {id: 'foo'}})).toDeny();
-  })
+  // await expect(db.collection('trips/public/posts').get()).toAllow();
 
-  test('Allow create trip with existing trip for yourself', async () => {
-    await expect(db.doc('trips-users/rst3').set({role: 'follower', user: {id: 'me'}, trip: {id: 'one'}})).toAllow();
-  })
 
-  test('Deny delete trip user of someone else', async () => {
-    await expect(db.doc('trips-users/xyz').delete()).toDeny();
-  })
 
-  test('Allow delete trip user of yourself', async () => {
-    await expect(db.doc('trips-users/abc').delete()).toAllow();
-  })
+  // test('Deny access random trip-user', async () => {
+  //   await expect(db.doc('trips-users/xyz').get()).toDeny();
+  // })
+  // test('Deny update random trip user', async () => {
+  //   await expect(db.doc('trips-users/xyz').update({role: 'follower'})).toDeny();
+  // })
+
+  // test('Allow getting own trip user', async () => {
+  //   await expect(db.doc('trips-users/abc').get()).toAllow();
+  // })
+  // test('Allow update own trip user', async () => {
+  //   await expect(db.doc('trips-users/abc').update({role: 'follower'})).toAllow();
+  // })
+  // test('Deny update user of own trip user', async () => {
+  //   await expect(db.doc('trips-users/abc').update({role: 'follower', user: {id: 'some'}})).toDeny();
+  // })
+  // test('Deny update trip of own trip user', async () => {
+  //   await expect(db.doc('trips-users/abc').update({role: 'follower', trip: {id: 'xyz'}})).toDeny();
+  // })
+
+  // test('Deny create trip for someone else', async () => {
+  //   await expect(db.doc('trips-users/hij').set({name: 'four', user: {id: 'someone'}})).toDeny();
+  // })
+  // test('Deny create trip without trip for yourself', async () => {
+  //   await expect(db.doc('trips-users/rst1').set({role: 'follower', user: {id: 'me'}})).toDeny();
+  // })
+  // test('Deny create trip with not existing trip for yourself', async () => {
+  //   await expect(db.doc('trips-users/rst2').set({role: 'follower', user: {id: 'me'}, trip: {id: 'foo'}})).toDeny();
+  // })
+
+  // test('Allow create trip with existing trip for yourself', async () => {
+  //   await expect(db.doc('trips-users/rst3').set({role: 'follower', user: {id: 'me'}, trip: {id: 'one'}})).toAllow();
+  // })
+
+  // test('Deny delete trip user of someone else', async () => {
+  //   await expect(db.doc('trips-users/xyz').delete()).toDeny();
+  // })
+
+  // test('Allow delete trip user of yourself', async () => {
+  //   await expect(db.doc('trips-users/abc').delete()).toAllow();
+  // })
 
 
 })
