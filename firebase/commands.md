a4ddd4b93d2a7d13657b9e030cdc84a6d525440a
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-08-06 23:29:19 +0200

    Local testing of Firebase functions

--- firebase/commands.md
+++ firebase/commands.md
@@ -2,3 +2,19 @@
 Run the emulator: firebase serve --only firestore
 
 Run the tests (from the firestore-tests folder): npm run test  or npm run test trips-users.spec.js
+
+
+# Firebase function testing #
+
+Run the emulator
+> firebase emulators:start
+
+Set environment flag
+> export FIRESTORE_EMULATOR_HOST=localhost:8080
+
+Run a test script (in terminal with environment set)
+> node ./functions/tests/test.js
+> node .tests.js
+
+Doing this will execute you code agains the emulator and Firestore functions will trigger
+This makes it possible to create unit tests
--- firebase/firestore-tests/spec/users.spec.js
+++ firebase/firestore-tests/spec/users.spec.js
@@ -35,30 +35,30 @@ describe('Not Authorized user', () => {
   })
 })
 
-describe('Authorized user', () => {
-  let db
-  // Applies only to tests in this describe block
-  beforeAll(async () => {
-    db = await setup(auth, mockData)
-  })
+// describe('Authorized user', () => {
+//   let db
+//   // Applies only to tests in this describe block
+//   beforeAll(async () => {
+//     db = await setup(auth, mockData)
+//   })
 
-  afterAll(async () => {
-    await teardown()
-  });
+//   afterAll(async () => {
+//     await teardown()
+//   });
 
-  test('Deny getting random user', async () => {
-    await expect(db.doc('users/other').get()).toDeny();
-  })
-  test('Deny update random user', async () => {
-    await expect(db.doc('users/other').update({name: 'name'})).toDeny();
-  })
+//   test('Deny getting random user', async () => {
+//     await expect(db.doc('users/other').get()).toDeny();
+//   })
+//   test('Deny update random user', async () => {
+//     await expect(db.doc('users/other').update({name: 'name'})).toDeny();
+//   })
 
-  test('Allow access own user', async () => {
-    await expect(db.doc('users/me').get()).toAllow();
-  })
-  test('Allow update own user', async () => {
-    await expect(db.doc('users/me').update({name: 'update'})).toAllow();
-  })
+//   test('Allow access own user', async () => {
+//     await expect(db.doc('users/me').get()).toAllow();
+//   })
+//   test('Allow update own user', async () => {
+//     await expect(db.doc('users/me').update({name: 'update'})).toAllow();
+//   })
 
-})
+// })
 
