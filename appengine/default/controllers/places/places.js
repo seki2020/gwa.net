629d622ec8124c6782ab88f4a731995f2106a2c3
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-09-07 22:44:01 +0200

    Nearby places endpoint

--- appengine/default/controllers/places/places.js
+++ appengine/default/controllers/places/places.js
@@ -1,8 +1,41 @@
+const got = require('got')
+const keys = require('../../secrets/keys')
+
 module.exports.nearBy = async function (req, res) {
+  let location = req.query.location;
+  if (!location) {
+    return res.end()
+  }
 
-  // This works
-  console.log(`Get NearBy`)
+  // Call remote to get nearby locations
+  let places = []
 
+	try {
+    // Foursquare implemenation. This is the best, but requires billing so need to implement alternatives
+    const url = `https://api.foursquare.com/v2/venues/search?ll=${location}&v=20190901&intent=checkin&limit=20&radius=150&client_id=${keys.foursquare.clientID}&client_secret=${keys.foursquare.clientSecret}`
+    const response = await got(url, {json: true})
+    if (response.statusCode == 200) {
+      let venues = response.body.response.venues
+      venues.forEach(venue => {
+        places.push({
+          'name': venue.name,
+          'address': venue.location.address,
+          'city': venue.location.city,
+          'country': venue.location.cc,
+          'location': {
+            'lat': venue.location.lat,
+            'lng': venue.location.lng
+          },
+          'distance': venue.location.distance
+        })
+      })
+    }
 
-  res.json({"foo": "bar"});
+  } catch (error) {
+		console.log(error.response.body);
+	}
+  res.json({
+    'source': 'foursquare',
+    'places': places,
+  });  
 }
