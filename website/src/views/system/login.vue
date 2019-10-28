103a51e72dda4bc5e5b4ddc73ef03e85e6b694c8
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-10-29 00:50:37 +0100

    Working without vee-validate

--- website/src/views/system/login.vue
+++ website/src/views/system/login.vue
@@ -9,28 +9,28 @@
               <form>
                 <div class="field">
                   <div class="control has-icons-left has-icons-right">
-                    <input type="email" name="email" class="input is-medium" :class="{'is-danger': errors.has('email') }" :placeholder="_('Email')" v-model.lazy="email" v-validate="'required|email'" >
+                    <input type="email" name="email" class="input is-medium"  :placeholder="_('Email')" v-model.lazy="email"  >
                     <span class="icon is-left">
                       <i class="fas fa-envelope"></i>
                     </span>
-                    <span v-show="fields.email && fields.email.valid" class="icon is-right" >
+                    <span v-show="false" class="icon is-right" >
                       <i class="fas fa-check"></i>
                     </span>
                   </div>
                 </div>
                 <div class="field">
                   <div class="control has-icons-left has-icons-right">
-                    <input type="password" name="password" class="input is-medium" :class="{'is-danger': errors.has('password') }" placeholder="Password" v-model="password" v-validate="'required|min:6'">
+                    <input type="password" name="password" class="input is-medium"  placeholder="Password" v-model="password" >
                     <span class="icon is-left">
                       <i class="fas fa-unlock-alt"></i>
                     </span>
-                    <span v-show="fields.password && fields.password.valid" class="icon is-right" >
+                    <span v-show="false" class="icon is-right" >
                       <i class="fas fa-check"></i>
                     </span>
                   </div>
                 </div>
                 <div class="field">
-                  <div class="message is-danger" v-if="hasErrors">
+                  <div class="message is-danger" v-if="false">
                     <div class="message-body is-size-7">
                       {{errorMessage}}
                     </div>
@@ -60,19 +60,19 @@ export default {
     }
   },
   computed: {
-    hasErrors () {
-      var errors = this.$validator.errors
-      return errors.has('email') || errors.has('password') || this.message
-    },
+    // hasErrors () {
+    //   var errors = this.$validator.errors
+    //   return errors.has('email') || errors.has('password') || this.message
+    // },
     errorMessage () {
-      var errors = this.$validator.errors
-      if (errors.has('email')) {
-        return errors.first('email')
-      } else if (errors.has('password')) {
-        return errors.first('password')
-      } else if (this.message) {
-        return this.message
-      }
+      // var errors = this.$validator.errors
+      // if (errors.has('email')) {
+      //   return errors.first('email')
+      // } else if (errors.has('password')) {
+      //   return errors.first('password')
+      // } else if (this.message) {
+      //   return this.message
+      // }
       return ''
     }
   },
@@ -85,35 +85,38 @@ export default {
       var router = this.$router
       // var store = this.$store
 
-      this.$validator.validate().then(valid => {
-        if (valid) {
-          console.log(' - Got valid form')
-          firebase.auth()
-            .signInWithEmailAndPassword(this.email, this.password)
-            .then(
-              user => {
-                console.log('Login: Got a user')
-                router.replace('/management')
-                // _user_.verify(user)
-                //   .then(function(data) {
-                //     console.log('Login: - verify: success')
-                //     store.dispatch('setUser', user);
-                //     router.replace('/');
-                //   })
-                //   .catch(function(error) {
-                //     console.log('Login: - verify: failure')
-                //     t.message = "This user is not registered"
+      // this.$validator.validate().then(valid => {
+      let valid = true
+      if (valid) {
+        console.log(' - Got valid form')
+        firebase.auth()
+          .signInWithEmailAndPassword(this.email, this.password)
+          .then(
+            user => {
+              debugger
+              console.log('Login: Got a user')
+              router.replace('/management')
+              // _user_.verify(user)
+              //   .then(function(data) {
+              //     console.log('Login: - verify: success')
+              //     store.dispatch('setUser', user);
+              //     router.replace('/');
+              //   })
+              //   .catch(function(error) {
+              //     console.log('Login: - verify: failure')
+              //     t.message = "This user is not registered"
 
-                //     // console.log(error)
-                //   })
-              },
-              error => {
-                this.message = error.message
-                console.log(' Error: ' + this.message)
-              }
-            )
-        }
-      })
+              //     // console.log(error)
+              //   })
+            },
+            error => {
+              debugger
+              this.message = error.message
+              console.log(' Error: ' + this.message)
+            }
+          )
+      }
+      // })
     }
   }
 }
