a17c8509935e264cc8836299a419534ee4b00ff7
Aad 't Hart <aad@aadjemonkeyrock.com>
2019-12-26 00:42:16 +0100

    Working FCM Messages

--- firebase/functions/db/users/notifications/onCreate.f.js
+++ firebase/functions/db/users/notifications/onCreate.f.js
@@ -8,17 +8,15 @@ exports = module.exports = functions.region('europe-west1').firestore
   .document('users/{userId}/notifications/{notificationId}')
   .onCreate((snapshot, context) => {
     const userId = context.params.userId
+    const notificationId = context.params.notificationId
 
     const notification = snapshot.data()
 
+    // Only type = 10 (post notification) are processed for now
     const type = notification.type
-    const user = notification.user.name
-
-    // Debugging
-    const tripName = notification.trip.name
-    const message = notification.post.message
-
-    console.log(`Notification: ${user} posted in ${tripName}: ${message}`)
+    if (type !== 10) {
+      return true
+    }
 
     // Get the user
     const db = admin.firestore()
@@ -29,39 +27,64 @@ exports = module.exports = functions.region('europe-west1').firestore
           throw new Error('User does not exist')
         }
         const userData = doc.data()
-        console.log(userData)
 
         // Get the tokens for this user
-        // const tokens = userData.fcmTokens
-        // if(tokens.length > 0) {
-        //   const message = {
-        //     notification: {
-        //       title: "Going walkabout",
-        //       body: notification.post.message
-        //     },
-        //     data: {
-        //       type: type,
-        //       tripId: notification.trip.id,
-        //       postId: notification.post.id,
-        //       userId: notification.user.id
-        //     },
-        //     tokens: tokens,
-        //   }
-          
-        //   admin.messaging().sendMulticast(message)
-        //     .then((response) => {
-        //       if (response.failureCount > 0) {
-        //         const failedTokens = [];
-        //         response.responses.forEach((resp, idx) => {
-        //           if (!resp.success) {
-        //             failedTokens.push(registrationTokens[idx]);
-        //           }
-        //         });
-        //         console.log('List of tokens that caused failures: ' + failedTokens);
-        //       }
-        //     });
-        // }
+        const tokens = userData.fcmTokens
+        if(tokens.length > 0) {
+          // No tokens, no notifications
+          var body = notification.trip.name + ': ' + notification.post.message
+          if (body.length > 120) {
+            body = body.substring(0,117) + "..."
+          }
 
+          const message = {
+            notification: {
+              title: notification.user.name,
+              body: body
+            },
+            data: {
+              type: type.toString(),
+              tripId: notification.trip.id,
+              postId: notification.post.id,
+              userId: notification.user.id,
+              notificationId
+            },
+            apns: {           // Apple specific settings
+              headers: {
+                  'apns-priority': '10'
+              },
+              payload: {
+                  aps: {
+                      sound: 'default'
+                      // badge: #unread messages
+                  }
+              },
+            },
+            // android: {
+            //   priority: 'high',
+            //   notification: {
+            //     sound: 'default',
+            //   }
+            // },
+            tokens: tokens,
+          }
+
+          return admin.messaging().sendMulticast(message)
+        }
+        return true
+      })
+      .then((response) => {
+        if (response.failureCount > 0) {
+          const failedTokens = [];
+          response.responses.forEach((resp, idx) => {
+            console.log(resp)
+            // if (!resp.success) {
+              // failedTokens.push(registrationTokens[idx]);
+            // }
+          });
+          console.log('List of tokens that caused failures: ' + failedTokens);
+        }
+        return
       })
       .catch(err => {
         console.error(err);
